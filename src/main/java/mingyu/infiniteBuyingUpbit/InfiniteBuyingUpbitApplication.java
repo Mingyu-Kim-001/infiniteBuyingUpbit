cc05a289fb078027ef62140e8471e05938d515ea
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2021-08-29 20:08:59 +0900

    initial project

---
+++ src/main/java/mingyu/infiniteBuyingUpbit/InfiniteBuyingUpbitApplication.java
@@ -0,0 +1,13 @@
+package mingyu.infiniteBuyingUpbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteBuyingUpbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiniteBuyingUpbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mingyu/infiniteBuyingUpbit/InfiniteBuyingUpbitApplicationTests.java
@@ -0,0 +1,13 @@
+package mingyu.infiniteBuyingUpbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteBuyingUpbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
